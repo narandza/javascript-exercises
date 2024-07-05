5ffd761e7bce475806a9637db2e5dede6f11206e
dimitrije <dimitrijejovanovic90210@gmail.com>
2024-07-05 08:37:09 +0200

    finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let reverseString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseString += str[i];
+  }
 
+  return reverseString;
 };
 
 // Do not edit below this line
