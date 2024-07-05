10bb8c9ef33e8b9f117924619a946ad21293d8a6
dimitrije <dimitrijejovanovic90210@gmail.com>
2024-07-05 10:37:24 +0200

    finish 05 excercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0 || typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";
+  }
 
+  const startRange = a < b ? a : b;
+  const endRange = a > b ? a : b;
+
+  console.log(startRange);
+  let sum = 0;
+
+  for (let i = startRange; i <= endRange; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
