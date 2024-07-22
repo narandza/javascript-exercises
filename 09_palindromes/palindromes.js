66740d10c3adbe92c11d0afb5d4d638b547e8d13
dimitrije <dimitrijejovanovic90210@gmail.com>
2024-07-22 17:54:35 +0200

    solve 09 excersice

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  str = str.toLowerCase();
+  const lettersRegEx = /[a-zA-z0-9]/;
+  const stripedStr = str.split("").filter((x) => x.match(lettersRegEx));
+  str = stripedStr.join("");
+  const reversed = stripedStr.reverse().join("");
 
+  return str === reversed;
 };
 
 // Do not edit below this line
